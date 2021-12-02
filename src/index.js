e83e9f4cc1d237b775938a2dd331c386cd79a959
Ana Ventura <anaventura1811@gmail.com>
2021-12-01 21:37:56 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// for parsing application/x-www-form-urlencoded
+// link for express docs: http://expressjs.com/pt-br/api.html#req.body
+
+app.get('/courses', (_req, res) => {
+  return res.json([
+    'JavaScript',
+    'TypeScript',
+    'RegEx',
+    'Docker',
+    'NodeJS',
+    'React'
+  ]);
+});
+
+app.post('/courses', (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  return res.json([
+    'JavaScript',
+    'TypeScript',
+    'RegEx',
+    'Docker',
+    'NodeJS',
+    'React',
+    'C#'
+  ])
+});
+
+app.put('/courses/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  return res.json([
+    'Fundamentos de JavaScript',
+    'TypeScript',
+    'RegEx',
+    'Docker',
+    'NodeJS',
+    'React',
+    'C#'
+  ]);
+});
+
+app.patch('/courses/:id', (_req, res) => {
+  return res.json([
+    'Fundamentos de JavaScript',
+    'TypeScript',
+    'RegEx',
+    'Docker',
+    'NodeJS',
+    'React',
+    'C# e C++'
+  ]);
+});
+
+app.delete('/courses/:id', (_req, res) => {
+  return res.json([
+    'Fundamentos de JavaScript',
+    'TypeScript',
+    'RegEx',
+    'Docker',
+    'NodeJS',
+    'C# e C++'
+  ]);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`ouvindo na porta ${PORT}`));
